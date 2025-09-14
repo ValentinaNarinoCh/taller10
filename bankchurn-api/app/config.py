e50b73a7396e2bc5a427f64df03dc374dcc40ae5
Ubuntu <ubuntu@ip-172-31-26-74.ec2.internal>
2025-09-14 00:15:34 +0000

    update project_name to include my name

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API"
+    PROJECT_NAME: str = "Bankchurn API - Valentina Nariño"
 
     class Config:
         case_sensitive = True
